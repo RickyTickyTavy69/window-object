742dcc12bc1c1cf75ac55a2fca06bc0536c1dc44
artem <bakirovartem69@gmail.com>
2022-07-14 18:33:48 +0200

    new commit

---
+++ assets/script/script.js
@@ -0,0 +1,19 @@
+/*alert("Hallo Welt");
+let answer = window.prompt("Bitte geben Sie Ihren Namnen ein!");
+console.log(answer);
+window.prompt("Bitte geben Sie Ihren Namen ein", "Cancel");
+let booleanAnswer = window.confirm("Stimmen Sie meiner Meinung zu?");
+console.log(booleanAnswer);*/
+/*
+let alter = prompt("gib mir bitte dein Alter");
+
+console.log(alter);
+
+let b = 5;
+let a = b * 5 - 3;
+alert(a);
+*/
+
+let mann = "Robert Wadlow ist der größte Mann der Welt : ";
+let grosse = "2,72m";
+alert(mann + grosse);
